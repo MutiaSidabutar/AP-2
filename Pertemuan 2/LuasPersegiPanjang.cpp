02181e079383cdd833428c39fe0083d208936ada
MutiaSidabutar <mutiasidabutar2006@gmail.com>
2025-03-14 15:29:21 +0700

    Pertemuan 2:Penggunaan Operator

---
+++ Pertemuan 2/LuasPersegiPanjang.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float luas,p,l;
+    cout<<"masukkan panjang : ";
+    cin>>p;
+    cout<<"masukkan lebar:";
+    cin>>l;
+    luas=p*l;
+    cout<<"luas  : "<<luas<<endl;
+
+}
