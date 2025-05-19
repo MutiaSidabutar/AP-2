84599adafffeb817b822230fbc3609745684bedd
MutiaSidabutar <mutiasidabutar2006@gmail.com>
2025-05-19 12:43:08 +0700

    Pertemuan 4: looping

---
+++ Pertemuan 4/loop.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //goto label
+    // hello world, fasilkom TI,Ilmu Komputer, IKLC
+    //-> hello world, IKLC, ilmu komputer, fasilkom TI
+    // a:
+    //     cout<<"hello world"<<endl;
+    //     goto d;
+    // b:
+    //     cout<<"Fasilkom TI"<<endl;
+    //     return 0;
+    // c:
+    //     cout<<"Ilmu Komputer"<<endl;
+    //     goto b;
+    // d:
+    //     cout<<"IKLC"<<endl;
+    //     goto c;
+        
+    // menampilkan bilangan genap dari 10 ->2 dengan goto
+//     int i=10;
+// genap:
+//     if(i%2==0){
+//         cout<<i<<" ";
+
+//     } i--;
+
+//     if(i>=2){
+//         goto genap;
+//     }
+
+//statement while
+// int i =1;
+// while(i<=10){
+//     if (i % 2==0){
+//         cout << i<<" ";
+
+//     } i++;
+// }
+
+    //Statement Do while
+    // int i = 1;
+    // do {
+    //     cout << 1<< endl;
+    // } while (1<=0);
+
+    // statement for
+    // for (insialisasi, kondisi, increase)
+    // for (int i=1; i<=10;i+=2){
+    //     cout<< "Hello World"<<endl;
+    // }
+
+    //Nested for
+    //***** 5x5 
+    // for(int i=1; i<=5; i++){
+    //     for(int j=1; j<=5; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //segitiga siku-siku
+    // for(int i=1; i<=5;i++){
+    //     for (int j=1;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+}
