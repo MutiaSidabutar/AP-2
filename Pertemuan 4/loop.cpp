df9be2611c698d35f1c347a6573e30c7a56fbf72
MutiaSidabutar <mutiasidabutar2006@gmail.com>
2025-05-25 08:47:24 +0700

    loop.cpp

--- Pertemuan 4/loop.cpp
+++ Pertemuan 4/loop.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    //goto label
+    //goto label : penggabungan dari goto dan statement
     // hello world, fasilkom TI,Ilmu Komputer, IKLC
     //-> hello world, IKLC, ilmu komputer, fasilkom TI
     // a:
@@ -30,7 +30,7 @@ int main(){
 //         goto genap;
 //     }
 
-//statement while
+//statement while : perulangan yang dilakukan sampai bernilai true
 // int i =1;
 // while(i<=10){
 //     if (i % 2==0){
@@ -39,19 +39,19 @@ int main(){
 //     } i++;
 // }
 
-    //Statement Do while
+    //Statement Do while :perulangan yang dilakukan walupun nilai awal false
     // int i = 1;
     // do {
     //     cout << 1<< endl;
     // } while (1<=0);
 
-    // statement for
+    // statement for : perulangan yang dilakukan ketika sudah ditentukan batasnya
     // for (insialisasi, kondisi, increase)
     // for (int i=1; i<=10;i+=2){
     //     cout<< "Hello World"<<endl;
     // }
 
-    //Nested for
+    //Nested for :perulangan yang dilakukan  untuk data dua dimensi atau matriks (2*2)
     //***** 5x5 
     // for(int i=1; i<=5; i++){
     //     for(int j=1; j<=5; j++){
